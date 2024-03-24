c9dd032df452b82891835425a2bff5118a1b0e50
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-24 18:34:17 +0200

    lastchanges

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const Main = () => {
-  const [counters, setCounters] = useState([-1]);
-
-  const incrementCounter = (index) => {
-    const updatedCounters = [...counters];
-
-    if (index == updatedCounters.length - 1) {
-      updatedCounters.push(-1);
-    } else {
-      updatedCounters[index] -= 1;
-    }
-    setCounters(updatedCounters);
-  };
+  const [counters, setCounters] = useState([[-1]]);
 
   const decrementCounter = (index) => {
     const updatedCounters = [...counters];
+  
+    if (index === updatedCounters.length - 1) {
+      updatedCounters.push([-1]);
+    } else if (Array.isArray(updatedCounters[index])) {
+      const lastElement = updatedCounters[index][updatedCounters[index].length - 1];
+      updatedCounters[index].push(lastElement - 1);
+    } else {
+      updatedCounters[index] = [-1]; 
+    }
+    
+    setCounters(updatedCounters);
+  };
+  const incrementCounter = (index) => {
+    const updatedCounters = [...counters];
     if (updatedCounters[index] < -1) {
       updatedCounters[index] += 1;
-    } else {
+    } else if (index > 0) {
       updatedCounters.pop();
     }
     setCounters(updatedCounters);
@@ -31,15 +34,19 @@ const Main = () => {
 
   return (
     <div>
-      {counters.map((counter, index) => (
-        <div
-          key={index}
-          className={`main flex  dynamic-margin`}
-          style={{ "--index": index + 1 }}
-        >
-          <div className="counter">{counter}</div>
-          <button onClick={() => incrementCounter(index)}>+</button>
-          <button onClick={() => decrementCounter(index)}>-</button>
+      {counters.map((sublist, index) => (
+        <div key={index} className="sublist">
+          {sublist.map((counter, sublistindex) => (
+            <div
+              key={sublistindex}
+              className={`main flex dynamic-margin`}
+              style={{ "--index": index + 1 }}
+            >
+              <div className="counter">{counter}</div>
+              <button onClick={() => decrementCounter(index)}>+</button>
+              <button onClick={() => incrementCounter(index)}>-</button>
+            </div>
+          ))}
         </div>
       ))}
     </div>
