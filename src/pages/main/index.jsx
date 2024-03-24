b903f642082a0890c82162d4e5850a2cba800e7e
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-24 15:29:35 +0200

    add component and delete when on click

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,24 +2,46 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const Main = () => {
-  const [counter, setCounter] = useState(0);
+  const [counters, setCounters] = useState([-1]);
 
-  const incrementCounter = () => {
-    setCounter(counter + 1);
+  const incrementCounter = (index) => {
+    const updatedCounters = [...counters];
+
+    if (index == updatedCounters.length - 1) {
+      updatedCounters.push(-1);
+    } else {
+      updatedCounters[index] -= 1;
+    }
+    setCounters(updatedCounters);
   };
-  const decrementCounter = () => {
-    setCounter(counter - 1);
-  
-  }
+
+  const decrementCounter = (index) => {
+    const updatedCounters = [...counters];
+    if (updatedCounters[index] < -1) {
+      updatedCounters[index] += 1;
+    } else {
+      updatedCounters.pop();
+    }
+    setCounters(updatedCounters);
+  };
+
   useEffect(() => {
-    console.log("Counter value:", counter);
-  }, [counter]);
+    console.log("Counter values:", counters);
+  }, [counters]);
 
   return (
-    <div className="main flex">
-      <div>{counter}</div>
-      <button onClick={incrementCounter}>+</button>
-      <button onClick={decrementCounter}>-</button>
+    <div>
+      {counters.map((counter, index) => (
+        <div
+          key={index}
+          className={`main flex  dynamic-margin`}
+          style={{ "--index": index + 1 }}
+        >
+          <div className="counter">{counter}</div>
+          <button onClick={() => incrementCounter(index)}>+</button>
+          <button onClick={() => decrementCounter(index)}>-</button>
+        </div>
+      ))}
     </div>
   );
 };
